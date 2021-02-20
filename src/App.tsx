a0001da0121d5e1a5639977f0d6fb3b727396c82
mostafa.drz <mostafa69d@gmail.com>
2021-02-20 15:44:32 -0500

    move it to context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
+import Slider from "./Slider";
 
 function App() {
   return (
     <div className="App">
-      <Slider>
+      <Slider className="slider">
         <SlideA />
         <SlideB />
         <SlideC />
@@ -14,95 +15,13 @@ function App() {
 }
 
 function SlideA(props: any) {
-  return (
-    <div>
-      Step A
-      <button onClick={props.handleNext} disabled={props.isLast}>
-        Next
-      </button>
-      <button onClick={props.handlePrevious} disabled={props.isFirst}>
-        Previous
-      </button>
-      <button onClick={props.handleGoToFirst} disabled={props.isFirst}>
-        Go To First
-      </button>
-      <button onClick={props.handleGoToLast} disabled={props.isLast}>
-        Go To Last
-      </button>
-    </div>
-  );
+  return <div>Step A</div>;
 }
 function SlideB(props: any) {
-  return (
-    <div>
-      Step B
-      <button onClick={props.handleNext} disabled={props.isLast}>
-        Next
-      </button>
-      <button onClick={props.handlePrevious} disabled={props.isFirst}>
-        Previous
-      </button>
-      <button onClick={props.handleGoToFirst} disabled={props.isFirst}>
-        Go To First
-      </button>
-      <button onClick={props.handleGoToLast} disabled={props.isLast}>
-        Go To Last
-      </button>
-    </div>
-  );
+  return <div>Step B</div>;
 }
 function SlideC(props: any) {
-  return (
-    <div>
-      Step C
-      <button onClick={props.handleNext} disabled={props.isLast}>
-        Next
-      </button>
-      <button onClick={props.handlePrevious} disabled={props.isFirst}>
-        Previous
-      </button>
-      <button onClick={props.handleGoToFirst} disabled={props.isFirst}>
-        Go To First
-      </button>
-      <button onClick={props.handleGoToLast} disabled={props.isLast}>
-        Go To Last
-      </button>
-    </div>
-  );
+  return <div>Step C</div>;
 }
-function Slider(props: { children: any }) {
-  const [step, setStep] = useState<number>(0);
-  const children = React.Children.toArray(props.children);
-  const current: any = children[step];
 
-  const handleNext = () => {
-    if (step >= children.length - 1) {
-      return;
-    }
-    setStep((s) => s + 1);
-  };
-  const handlePrevious = () => {
-    if (step <= 0) {
-      return;
-    }
-    setStep((s) => s - 1);
-  };
-  const handleGoToFirst = () => {
-    setStep(0);
-  };
-  const handleGoToLast = () => {
-    setStep(children.length - 1);
-  };
-  let elementWithClassName = React.cloneElement(current, {
-    handleNext,
-    handlePrevious,
-    handleGoToFirst,
-    handleGoToLast,
-    step,
-    isLast: step === children.length - 1,
-    isFirst: step === 0,
-  });
-
-  return elementWithClassName;
-}
 export default App;
---
+++ src/Slider.tsx
@@ -0,0 +1,100 @@
+import React, { useState, useCallback } from "react";
+
+interface ISlider {
+  handleNext: () => void;
+  handlePrevious: () => void;
+  handleGoToFirst: () => void;
+  handleGoToLast: () => void;
+  step: number;
+  isLast: boolean;
+  isFirst: boolean;
+}
+const SliderContext = React.createContext<ISlider>({
+  handleNext: () => {},
+  handlePrevious: () => {},
+  handleGoToFirst: () => {},
+  handleGoToLast: () => {},
+  step: 0,
+  isLast: false,
+  isFirst: false,
+});
+
+export const useSlider = () => {
+  const sliderContext = React.useContext(SliderContext);
+  if (!sliderContext) {
+    throw Error(
+      "You can't use slider without the Slider Provider. Wrap your components with the Slider.Provider"
+    );
+  }
+  return sliderContext;
+};
+
+function Slider(props: any) {
+  const [step, setStep] = useState<number>(0);
+  const children = React.Children.toArray(props.children);
+  const current: any = children[step];
+
+  const handleNext = () => {
+    if (step >= children.length - 1) {
+      return;
+    }
+    setStep((s) => s + 1);
+  };
+  const handlePrevious = useCallback(() => {
+    if (step <= 0) {
+      return;
+    }
+    setStep((s) => s - 1);
+  }, [step]);
+  const handleGoToFirst = useCallback(() => {
+    setStep(0);
+  }, []);
+  const handleGoToLast = useCallback(() => {
+    setStep(children.length - 1);
+  }, [children.length]);
+
+  return (
+    <SliderContext.Provider
+      value={{
+        handleNext,
+        handlePrevious,
+        handleGoToFirst,
+        handleGoToLast,
+        step,
+        isFirst: step === 0,
+        isLast: step === children.length - 1,
+      }}
+    >
+      {current}
+      <SliderToolbar />
+    </SliderContext.Provider>
+  );
+}
+
+function SliderToolbar() {
+  const {
+    handleNext,
+    handleGoToFirst,
+    handleGoToLast,
+    handlePrevious,
+    isFirst,
+    isLast,
+  } = useSlider();
+  return (
+    <div>
+      <button onClick={handleNext} disabled={isLast}>
+        Next
+      </button>
+      <button onClick={handlePrevious} disabled={isFirst}>
+        Previous
+      </button>
+      <button onClick={handleGoToFirst} disabled={isFirst}>
+        Go To First
+      </button>
+      <button onClick={handleGoToLast} disabled={isLast}>
+        Go To Last
+      </button>
+    </div>
+  );
+}
+export default Slider;
